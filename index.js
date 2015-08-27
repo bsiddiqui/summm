300115de00c3fc6e5f6d8c8dbb3252c0688e1a92
Basil Siddiqui <basil.siddiqui@gmail.com>
2015-08-27 15:33:12 -0700

    Check numeric with isNumeric, isNaN doesn't catch booleans

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 'use strict'
 
+var isNumeric = require('is-numeric')
+
 module.exports = function summm (value1, value2) {
-  if (isNaN(value1) || isNaN(value2)) {
+  if (!isNumeric(value1) || !isNumeric(value2)) {
     throw new Error('value1 and value2 must be a number')
   }
 
